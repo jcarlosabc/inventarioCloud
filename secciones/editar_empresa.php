a508790c4897af679aae9e57bf86803d30350aac
Srwonkka <juansebastianc419@gmail.com>
2024-04-17 05:21:28 -0500

    Fix: editar empresa bodega -trasladar productos bodega ADD:  nuevas columnas en index clientes

--- secciones/editar_empresa.php
+++ secciones/editar_empresa.php
@@ -1,20 +1,25 @@
 <?php include("../templates/header.php") ?>
 <?php
 if (isset($_GET['txtID'])) {
-
     $txtID = (isset($_GET['txtID'])) ? $_GET['txtID'] : "";
-    $datos_empresa = $conexion->prepare("SELECT * FROM empresa WHERE empresa_id = :empresa_id");
+
+    $datos_empresa = $conexion->prepare("SELECT *, 'empresa' AS origen FROM empresa WHERE empresa_id = :empresa_id
+                                         UNION
+                                         SELECT *, 'empresa_bodega' AS origen FROM empresa_bodega WHERE bodega_id = :empresa_id");
 
     $datos_empresa->bindParam(":empresa_id", $txtID);
     $datos_empresa->execute();
     $registro = $datos_empresa->fetch(PDO::FETCH_ASSOC);
 
-    $empresa_nombre = $registro['empresa_nombre'];
-    $empresa_telefono = $registro['empresa_telefono'];
-    $empresa_direccion = $registro['empresa_direccion'];
-    $empresa_nit = $registro['empresa_nit'];
-    $codigo_seguridad = $registro['codigo_seguridad'];
-    $link = $registro['link'];
+    if ($registro) {
+        $empresa_nombre = $registro['empresa_nombre'];
+        $empresa_telefono = $registro['empresa_telefono'];
+        $empresa_direccion = $registro['empresa_direccion'];
+        $empresa_nit = $registro['empresa_nit'];
+        $codigo_seguridad = $registro['codigo_seguridad'];
+        $link = $registro['link'];
+        $origen = $registro['origen'];
+    }
 }
 
 if ($_POST) {
@@ -26,16 +31,15 @@ if ($_POST) {
     $email_empresa = isset($_POST['email_empresa']) ? $_POST['email_empresa'] : "";
     $direccion_empresa = isset($_POST['direccion_empresa']) ? $_POST['direccion_empresa'] : "";
     $responsable = isset($_SESSION['usuario_id']) ?  $_SESSION['usuario_id']  : 0;
-    $nueva_clave = isset($_POST['nueva_clave']) ? $_POST['nueva_clave'] : "";
+    $nueva_clave = $_POST['nueva_clave'];
     $clave_info = $nueva_clave;
 
     //link_empresa 
     $link = isset($_POST['link']) ?   $_POST['link']  : " ";
     $codigo_seguridad = isset($_POST['codigo_seguridad']) ? $_POST['codigo_seguridad'] : " ";   
 
-
-    //  GUARDANDO LA EMPRSA CREADA
-    $sql = "UPDATE empresa SET empresa_nombre = :nombre, empresa_telefono = :telefono, empresa_email = :email, empresa_direccion = :direccion, empresa_nit = :nit, link = :link, codigo_seguridad = :codigo_seguridad, responsable = :responsable WHERE empresa_id = :empresa_id";
+    if ($txtID == 9000) {
+        $sql = "UPDATE empresa_bodega SET bodega_nombre = :nombre, bodega_telefono = :telefono, bodega_email = :email, bodega_direccion = :direccion, bodega_nit = :nit, link = :link, codigo_seguridad = :codigo_seguridad, responsable = :responsable WHERE bodega_id = :empresa_id";
 $sentencia = $conexion->prepare($sql);
 $params = array(
     ':nombre' => $nombre_empresa,
@@ -49,6 +53,24 @@ $params = array(
     ':empresa_id' => $txtID
 );
 $sentencia->execute($params);
+    }else {
+        //  GUARDANDO LA EMPRSA CREADA
+        $sql = "UPDATE empresa SET empresa_nombre = :nombre, empresa_telefono = :telefono, empresa_email = :email, empresa_direccion = :direccion, empresa_nit = :nit, link = :link, codigo_seguridad = :codigo_seguridad, responsable = :responsable WHERE empresa_id = :empresa_id";
+    $sentencia = $conexion->prepare($sql);
+    $params = array(
+        ':nombre' => $nombre_empresa,
+        ':telefono' => $telefono_empresa,
+        ':email' => $email_empresa,
+        ':direccion' => $direccion_empresa,
+        ':nit' => $nit_empresa,
+        ':link' => $link,
+        ':codigo_seguridad' => $codigo_seguridad,
+        ':responsable' => $responsable,
+        ':empresa_id' => $txtID
+    );
+    $sentencia->execute($params);
+
+    }
 
     // GUARDAR USUARIO DE LA EMPRESA
     $sql = "UPDATE usuario SET usuario_nombre = :nombre, usuario_apellido = :apellido, usuario_telefono = :telefono, usuario_email = :email, usuario_usuario = :usuario, usuario_clave = :clave, info_clave = :clave_info, rol = :rol, usuario_foto = :foto, caja_id = :caja_id, responsable = :responsable WHERE link = :link";
@@ -137,7 +159,7 @@ $resultado = $sentencia_usuario->execute($params);
                     <div class="col-sm-3">
                         <div class="form-group">
                             <label class="textLabel">Contraseña de Usuario</label> &nbsp;<i class="nav-icon fas fa-edit"></i>
-                            <input type="password" class="form-control camposTabla"name="nueva_clave" required>
+                            <input type="password" class="form-control camposTabla"name="nueva_clave">
                         </div>
                     </div>
                     <div class="col-sm-3">
